cc7dc67f9aa7604e3c31550cb1c66e83c4c13921
David Albalate Cabedo <david@netshell.co.uk>
2020-01-19 11:35:25 +0000

    Lecture 25

---
+++ DatingApp.API/DatingApp.API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Username { get; set; }
+        public int PasswordHash { get; set; }
+        public int PasswordSalt { get; set; }
+    }
+}
