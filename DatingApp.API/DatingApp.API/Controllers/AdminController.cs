9b29f00dcabeeca57da6cd99752c598946581212
David Albalate Cabedo <capelo39@gmail.com>
2020-04-13 12:21:38 +0100

    Lecture 221 Añadido admin controler

---
+++ DatingApp.API/DatingApp.API/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly DataContext context;
+        private readonly UserManager<User> userManager;
+
+        public AdminController(DataContext context, UserManager<User> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("usersWithRoles")]
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            var userList = await this.context.Users
+                .OrderBy(x => x.UserName)
+                .Select(user => new
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = (from userRole in user.UserRoles
+                             join role in this.context.Roles
+                             on userRole.RoleId
+                             equals role.Id
+                             select role.Name).ToList()
+                }).ToListAsync();
+            return Ok(userList);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("editRoles/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEdit roleEdit)
+        {
+            var user = await this.userManager.FindByNameAsync(userName);
+            var userRoles = await this.userManager.GetRolesAsync(user);
+            var selectedRoles = roleEdit.RoleNames;
+
+            // Esto equivale a selectedRoles = selectedRoles != null ? selectedRoles : new string[] {};
+            selectedRoles = selectedRoles ?? new string[] {};
+            var result = await this.userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+            
+
+            if (!result.Succeeded)
+                return BadRequest("Failed To Add to roles");
+
+            result = await this.userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+                return BadRequest("Failed To Remove the roles");
+
+            return Ok(await this.userManager.GetRolesAsync(user));
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photosForModeration")]
+        public IActionResult GetPhotosForModerate()
+        {
+            return Ok("Get Photos For Moderate");
+        }
+    }
+}
